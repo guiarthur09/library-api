053afaff1011354308ef0c1b3c8a26ac029a1674
Guilherme <guilhermearthursilveira13@gmail.com>
2026-07-23 19:47:25 -0300

    fix: correca de bugs, a logica e a conexao com o SQLite3

--- routes/books.py
+++ routes/books.py
@@ -1,25 +1,8 @@
-from models.books import LivroCreate, Livro
+from database.connection import connection, cursor
+from models.books import BookCreate, Book
 from fastapi import APIRouter, HTTPException
 
 router = APIRouter()
-
-livros = [
-    {
-        "id": 1,
-        "titulo": "Python para Iniciantes",
-        "autor": "João Silva"
-    },
-    {
-        "id": 2,
-        "titulo": "FastAPI na Prática",
-        "autor": "Maria Souza"
-    },
-    {
-        "id": 3,
-        "titulo": "Banco de Dados",
-        "autor": "Carlos Lima"
-    }
-]
 
 @router.get("/")
 def home():
@@ -27,48 +10,60 @@ def home():
         "mensagem": "Bem-vindo à API da Biblioteca!"
     }
 
-@router.post("/books", response_model=Livro)
-def criar_livro(livro: LivroCreate):
+@router.post("/books", response_model=Book)
+def criar_livro(livro: BookCreate):
 
-    novo_id = len(livros) + 1
+    cursor.execute("""INSERT INTO books (title, author, publisher, location, year, pages)
+    VALUES (?, ?, ?, ?, ?, ?)""", (livro.title, livro.author, livro.publisher, livro.location, livro.year, livro.pages))
 
-    novo_livro = {
-        "id": novo_id,
-        "titulo": livro.titulo,
-        "autor": livro.autor
-    }
+    connection.commit()
 
-    livros.append(novo_livro)
-
-    return novo_livro
+    novo_id = cursor.lastrowid
+    return {**livro.model_dump(), "id": novo_id}
 
 @router.get("/books")
 def listar_livros():
-    return livros
+
+    cursor.execute("""SELECT * FROM books""")
+
+    livros = cursor.fetchall()
+    return [dict(livro) for livro in livros]
 
 
 @router.get("/books/{id}")
 def buscar_livro(id: int):
-    for livro in livros:
-        if livro["id"] == id:
-            return livro
-    raise HTTPException(status_code=404, detail="404 Not Found")
 
-@router.put("/books/{id}", response_model=Livro)
-def atualizar_livro(id: int, livro_atualizado: LivroCreate):
-    for livro in livros:
-        if livro["id"] == id:
-            livro["titulo"] = livro_atualizado.titulo
-            livro["autor"] = livro_atualizado.autor
-            return livro
+   cursor.execute("""SELECT * FROM books WHERE id= ?""", (id,))
 
-    raise HTTPException(status_code=404, detail="404 Not Found")
+   livro = cursor.fetchone()
+   if livro:
+       return dict(livro)
+
+   raise HTTPException(status_code=404, detail="404 Not Found")
+
+@router.put("/books/{id}", response_model=Book)
+def atualizar_livro(id: int, livro: BookCreate):
+
+    cursor.execute("""UPDATE books
+                      SET title=?, author=?,
+                      publisher=?, location=?,
+                      year=?, pages=?
+                      WHERE id=?""", (livro.title, livro.author, livro.publisher, livro.location, livro.year, livro.pages, id))
+
+    connection.commit()
+
+    if cursor.rowcount == 0:
+        raise HTTPException(status_code=404, detail="404 Not Found")
+
+    return {**livro.model_dump(), "id": id}
 
 @router.delete("/books/{id}")
 def deletar_livro(id: int):
-    for index, livro in enumerate(livros):
-        if livro["id"] == id:
-            del livros[index]
-            return {"deleted": f"ID: {livro['id']} | Name: {livro['titulo']} | Autor: {livro['autor']}"}
-        
-    raise HTTPException(status_code=404, detail="404 Not Found")
+
+    cursor.execute("""DELETE FROM books WHERE id=?""", (id,))
+    connection.commit()
+
+    if cursor.rowcount == 0:
+        raise HTTPException(status_code=404, detail="404 Not Found")
+
+    return {"deleted": f"ID: {id}"}
